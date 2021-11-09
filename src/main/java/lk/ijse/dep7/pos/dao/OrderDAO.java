e0a78911cf93ec8c3b3fd37cd8ab43b45c687803
Ranjith Suranga <d.m.ranjith.sura@gmail.com>
2021-11-09 18:30:11 +0530

    Add basic CRUD operations to all DAOs

--- src/main/java/lk/ijse/dep7/pos/dao/OrderDAO.java
+++ src/main/java/lk/ijse/dep7/pos/dao/OrderDAO.java
@@ -1,11 +1,8 @@
 package lk.ijse.dep7.pos.dao;
 
-import lk.ijse.dep7.pos.dto.OrderDTO;
-import lk.ijse.dep7.pos.dto.OrderDetailDTO;
+import lk.ijse.dep7.pos.entity.Order;
 
-import java.sql.*;
-import java.time.LocalDate;
-import java.util.ArrayList;
+import java.sql.Connection;
 import java.util.List;
 
 public class OrderDAO {
@@ -16,197 +13,226 @@ public class OrderDAO {
         this.connection = connection;
     }
 
-    public boolean existsOrder(String orderId) {
-        try {
-            PreparedStatement stm = connection.prepareStatement("SELECT id FROM `order` WHERE id=?");
-            stm.setString(1, orderId);
-            return stm.executeQuery().next();
-        } catch (Exception e) {
-            throw new RuntimeException("Failed operation exception", e);
-        }
+    public void saveOrder(Order order) {
+
     }
 
-    public void saveOrder(String orderId, LocalDate orderDate, String customerId) {
-        try {
-            PreparedStatement stm = connection.prepareStatement("INSERT INTO `order` (id, date, customer_id) VALUES (?,?,?)");
-            stm.setString(1, orderId);
-            stm.setDate(2, Date.valueOf(orderDate));
-            stm.setString(3, customerId);
-            stm.executeUpdate();
-        } catch (SQLException e) {
-            throw new RuntimeException("Failed to save the order", e);
-        }
+    public void updateOrder(Order order) {
+
     }
 
-    public void saveOrderDetail(String orderId, OrderDetailDTO orderDetail) {
-        try {
-            PreparedStatement stm = connection.prepareStatement("INSERT INTO order_detail (order_id, item_code, unit_price, qty) VALUES (?,?,?,?)");
-            stm.setString(1, orderId);
-            stm.setString(2, orderDetail.getItemCode());
-            stm.setBigDecimal(3, orderDetail.getUnitPrice());
-            stm.setInt(4, orderDetail.getQty());
-            stm.executeUpdate();
-        } catch (SQLException e) {
-            throw new RuntimeException("Failed to save the order detail", e);
-        }
+    public void deleteOrderById(String orderId) {
+
     }
 
-    public List<OrderDTO> searchOrders(String query) {
-        List<OrderDTO> orderList = new ArrayList<>();
-
-        try {
-            String[] searchWords = query.split("\\s");
-            StringBuilder sqlBuilder = new StringBuilder("SELECT o.*, c.name, order_total.total\n" +
-                    "FROM `order` o\n" +
-                    "         INNER JOIN customer c on o.customer_id = c.id\n" +
-                    "         INNER JOIN\n" +
-                    "     (SELECT order_id, SUM(qty * unit_price) AS total FROM order_detail od GROUP BY order_id) AS order_total\n" +
-                    "     ON o.id = order_total.order_id\n" +
-                    "WHERE (order_id LIKE ?\n" +
-                    "    OR date LIKE ?\n" +
-                    "    OR customer_id LIKE ?\n" +
-                    "    OR name LIKE ?) ");
-
-            for (int i = 1; i < searchWords.length; i++) {
-                sqlBuilder.append("AND (\n" +
-                        "            order_id LIKE ?\n" +
-                        "        OR date LIKE ?\n" +
-                        "        OR customer_id LIKE ?\n" +
-                        "        OR name LIKE ?)");
-            }
-            PreparedStatement stm = connection.prepareStatement(sqlBuilder.toString());
-
-            for (int i = 0; i < searchWords.length * 4; i++) {
-                stm.setString(i + 1, "%" + searchWords[(i / 4)] + "%");
-            }
-            ResultSet rst = stm.executeQuery();
-
-            while (rst.next()) {
-                orderList.add(new OrderDTO(rst.getString("id"), rst.getDate("date").toLocalDate(),
-                        rst.getString("customer_id"), rst.getString("name"), rst.getBigDecimal("total")));
-            }
-
-            return orderList;
-        } catch (SQLException e) {
-            throw new RuntimeException("Failed to search orders", e);
-        }
+    public Order findOrderById(String orderId) {
+        return null;
     }
 
-    public long getSearchOrderCount(String query) {
-        try {
-            String[] searchWords = query.split("\\s");
-            StringBuilder sqlBuilder = new StringBuilder("SELECT COUNT(*) \n" +
-                    "FROM `order` o\n" +
-                    "         INNER JOIN customer c on o.customer_id = c.id\n" +
-                    "         INNER JOIN\n" +
-                    "     (SELECT order_id, SUM(qty * unit_price) AS total FROM order_detail od GROUP BY order_id) AS order_total\n" +
-                    "     ON o.id = order_total.order_id\n" +
-                    "WHERE (order_id LIKE ?\n" +
-                    "    OR date LIKE ?\n" +
-                    "    OR customer_id LIKE ?\n" +
-                    "    OR name LIKE ?) ");
-
-            for (int i = 1; i < searchWords.length; i++) {
-                sqlBuilder.append("AND (\n" +
-                        "            order_id LIKE ?\n" +
-                        "        OR date LIKE ?\n" +
-                        "        OR customer_id LIKE ?\n" +
-                        "        OR name LIKE ?)");
-            }
-            PreparedStatement stm = connection.prepareStatement(sqlBuilder.toString());
-
-            for (int i = 0; i < searchWords.length * 4; i++) {
-                stm.setString(i + 1, "%" + searchWords[(i / 4)] + "%");
-            }
-
-            ResultSet rst = stm.executeQuery();
-            rst.next();
-            return rst.getLong(1);
-        } catch (SQLException e) {
-            throw new RuntimeException("Failed to get the count", e);
-        }
+    public List<Order> findAllOrders() {
+        return null;
     }
 
-    public List<OrderDTO> searchOrders(String query, int page, int size) {
-        List<OrderDTO> orderList = new ArrayList<>();
-
-        try {
-            String[] searchWords = query.split("\\s");
-            StringBuilder sqlBuilder = new StringBuilder("SELECT o.*, c.name, order_total.total\n" +
-                    "FROM `order` o\n" +
-                    "         INNER JOIN customer c on o.customer_id = c.id\n" +
-                    "         INNER JOIN\n" +
-                    "     (SELECT order_id, SUM(qty * unit_price) AS total FROM order_detail od GROUP BY order_id) AS order_total\n" +
-                    "     ON o.id = order_total.order_id\n" +
-                    "WHERE (order_id LIKE ?\n" +
-                    "    OR date LIKE ?\n" +
-                    "    OR customer_id LIKE ?\n" +
-                    "    OR name LIKE ?) ");
-
-            for (int i = 1; i < searchWords.length; i++) {
-                sqlBuilder.append("AND (\n" +
-                        "            order_id LIKE ?\n" +
-                        "        OR date LIKE ?\n" +
-                        "        OR customer_id LIKE ?\n" +
-                        "        OR name LIKE ?)");
-            }
-            sqlBuilder.append(" LIMIT ? OFFSET ?");
-            PreparedStatement stm = connection.prepareStatement(sqlBuilder.toString());
-
-            for (int i = 0; i < searchWords.length * 4; i++) {
-                stm.setString(i + 1, "%" + searchWords[(i / 4)] + "%");
-            }
-            stm.setInt((searchWords.length * 4) + 1, size);
-            stm.setInt((searchWords.length * 4) + 2, size * (page - 1));
-            ResultSet rst = stm.executeQuery();
-
-            while (rst.next()) {
-                orderList.add(new OrderDTO(rst.getString("id"), rst.getDate("date").toLocalDate(),
-                        rst.getString("customer_id"), rst.getString("name"), rst.getBigDecimal("total")));
-            }
-
-            return orderList;
-        } catch (SQLException e) {
-            throw new RuntimeException("Failed to search orders", e);
-        }
+    public long countOrders() {
+        return 0;
     }
 
-    public List<OrderDetailDTO> findOrderDetails(String orderId) {
-        List<OrderDetailDTO> orderDetailsList = new ArrayList<>();
-
-        try {
-            PreparedStatement stm = connection.prepareStatement("SELECT id FROM `order` WHERE id=?");
-            stm.setString(1, orderId);
-
-            if (!stm.executeQuery().next()) throw new RuntimeException("Invalid order id");
-
-            stm = connection.prepareStatement("SELECT * FROM order_detail WHERE order_id=?");
-            stm.setString(1, orderId);
-            ResultSet rst = stm.executeQuery();
-
-            while (rst.next()) {
-                orderDetailsList.add(new OrderDetailDTO(rst.getString("item_code"),
-                        rst.getInt("qty"),
-                        rst.getBigDecimal("unit_price")));
-            }
-
-            return orderDetailsList;
-        } catch (SQLException e) {
-            throw new RuntimeException("Failed fetch order details for order id: " + orderId, e);
-        }
+    public boolean existsOrderById(String orderId) {
+        return false;
     }
 
-    public String getLastOrderId() {
-        try {
-            ResultSet rst = connection.createStatement().executeQuery("SELECT id FROM `order` ORDER BY id DESC LIMIT 1;");
 
-            if (rst.next()) {
-                return rst.getString("id");
-            } else {
-                return null;
-            }
-        } catch (Exception e) {
-            throw new RuntimeException("Failed operation");
-        }
-    }
+//    public boolean existsOrder(String orderId) {
+//        try {
+//            PreparedStatement stm = connection.prepareStatement("SELECT id FROM `order` WHERE id=?");
+//            stm.setString(1, orderId);
+//            return stm.executeQuery().next();
+//        } catch (Exception e) {
+//            throw new RuntimeException("Failed operation exception", e);
+//        }
+//    }
+//
+//    public void saveOrder(String orderId, LocalDate orderDate, String customerId) {
+//        try {
+//            PreparedStatement stm = connection.prepareStatement("INSERT INTO `order` (id, date, customer_id) VALUES (?,?,?)");
+//            stm.setString(1, orderId);
+//            stm.setDate(2, Date.valueOf(orderDate));
+//            stm.setString(3, customerId);
+//            stm.executeUpdate();
+//        } catch (SQLException e) {
+//            throw new RuntimeException("Failed to save the order", e);
+//        }
+//    }
+//
+//    public void saveOrderDetail(String orderId, OrderDetailDTO orderDetail) {
+//        try {
+//            PreparedStatement stm = connection.prepareStatement("INSERT INTO order_detail (order_id, item_code, unit_price, qty) VALUES (?,?,?,?)");
+//            stm.setString(1, orderId);
+//            stm.setString(2, orderDetail.getItemCode());
+//            stm.setBigDecimal(3, orderDetail.getUnitPrice());
+//            stm.setInt(4, orderDetail.getQty());
+//            stm.executeUpdate();
+//        } catch (SQLException e) {
+//            throw new RuntimeException("Failed to save the order detail", e);
+//        }
+//    }
+//
+//    public List<OrderDTO> searchOrders(String query) {
+//        List<OrderDTO> orderList = new ArrayList<>();
+//
+//        try {
+//            String[] searchWords = query.split("\\s");
+//            StringBuilder sqlBuilder = new StringBuilder("SELECT o.*, c.name, order_total.total\n" +
+//                    "FROM `order` o\n" +
+//                    "         INNER JOIN customer c on o.customer_id = c.id\n" +
+//                    "         INNER JOIN\n" +
+//                    "     (SELECT order_id, SUM(qty * unit_price) AS total FROM order_detail od GROUP BY order_id) AS order_total\n" +
+//                    "     ON o.id = order_total.order_id\n" +
+//                    "WHERE (order_id LIKE ?\n" +
+//                    "    OR date LIKE ?\n" +
+//                    "    OR customer_id LIKE ?\n" +
+//                    "    OR name LIKE ?) ");
+//
+//            for (int i = 1; i < searchWords.length; i++) {
+//                sqlBuilder.append("AND (\n" +
+//                        "            order_id LIKE ?\n" +
+//                        "        OR date LIKE ?\n" +
+//                        "        OR customer_id LIKE ?\n" +
+//                        "        OR name LIKE ?)");
+//            }
+//            PreparedStatement stm = connection.prepareStatement(sqlBuilder.toString());
+//
+//            for (int i = 0; i < searchWords.length * 4; i++) {
+//                stm.setString(i + 1, "%" + searchWords[(i / 4)] + "%");
+//            }
+//            ResultSet rst = stm.executeQuery();
+//
+//            while (rst.next()) {
+//                orderList.add(new OrderDTO(rst.getString("id"), rst.getDate("date").toLocalDate(),
+//                        rst.getString("customer_id"), rst.getString("name"), rst.getBigDecimal("total")));
+//            }
+//
+//            return orderList;
+//        } catch (SQLException e) {
+//            throw new RuntimeException("Failed to search orders", e);
+//        }
+//    }
+//
+//    public long getSearchOrderCount(String query) {
+//        try {
+//            String[] searchWords = query.split("\\s");
+//            StringBuilder sqlBuilder = new StringBuilder("SELECT COUNT(*) \n" +
+//                    "FROM `order` o\n" +
+//                    "         INNER JOIN customer c on o.customer_id = c.id\n" +
+//                    "         INNER JOIN\n" +
+//                    "     (SELECT order_id, SUM(qty * unit_price) AS total FROM order_detail od GROUP BY order_id) AS order_total\n" +
+//                    "     ON o.id = order_total.order_id\n" +
+//                    "WHERE (order_id LIKE ?\n" +
+//                    "    OR date LIKE ?\n" +
+//                    "    OR customer_id LIKE ?\n" +
+//                    "    OR name LIKE ?) ");
+//
+//            for (int i = 1; i < searchWords.length; i++) {
+//                sqlBuilder.append("AND (\n" +
+//                        "            order_id LIKE ?\n" +
+//                        "        OR date LIKE ?\n" +
+//                        "        OR customer_id LIKE ?\n" +
+//                        "        OR name LIKE ?)");
+//            }
+//            PreparedStatement stm = connection.prepareStatement(sqlBuilder.toString());
+//
+//            for (int i = 0; i < searchWords.length * 4; i++) {
+//                stm.setString(i + 1, "%" + searchWords[(i / 4)] + "%");
+//            }
+//
+//            ResultSet rst = stm.executeQuery();
+//            rst.next();
+//            return rst.getLong(1);
+//        } catch (SQLException e) {
+//            throw new RuntimeException("Failed to get the count", e);
+//        }
+//    }
+//
+//    public List<OrderDTO> searchOrders(String query, int page, int size) {
+//        List<OrderDTO> orderList = new ArrayList<>();
+//
+//        try {
+//            String[] searchWords = query.split("\\s");
+//            StringBuilder sqlBuilder = new StringBuilder("SELECT o.*, c.name, order_total.total\n" +
+//                    "FROM `order` o\n" +
+//                    "         INNER JOIN customer c on o.customer_id = c.id\n" +
+//                    "         INNER JOIN\n" +
+//                    "     (SELECT order_id, SUM(qty * unit_price) AS total FROM order_detail od GROUP BY order_id) AS order_total\n" +
+//                    "     ON o.id = order_total.order_id\n" +
+//                    "WHERE (order_id LIKE ?\n" +
+//                    "    OR date LIKE ?\n" +
+//                    "    OR customer_id LIKE ?\n" +
+//                    "    OR name LIKE ?) ");
+//
+//            for (int i = 1; i < searchWords.length; i++) {
+//                sqlBuilder.append("AND (\n" +
+//                        "            order_id LIKE ?\n" +
+//                        "        OR date LIKE ?\n" +
+//                        "        OR customer_id LIKE ?\n" +
+//                        "        OR name LIKE ?)");
+//            }
+//            sqlBuilder.append(" LIMIT ? OFFSET ?");
+//            PreparedStatement stm = connection.prepareStatement(sqlBuilder.toString());
+//
+//            for (int i = 0; i < searchWords.length * 4; i++) {
+//                stm.setString(i + 1, "%" + searchWords[(i / 4)] + "%");
+//            }
+//            stm.setInt((searchWords.length * 4) + 1, size);
+//            stm.setInt((searchWords.length * 4) + 2, size * (page - 1));
+//            ResultSet rst = stm.executeQuery();
+//
+//            while (rst.next()) {
+//                orderList.add(new OrderDTO(rst.getString("id"), rst.getDate("date").toLocalDate(),
+//                        rst.getString("customer_id"), rst.getString("name"), rst.getBigDecimal("total")));
+//            }
+//
+//            return orderList;
+//        } catch (SQLException e) {
+//            throw new RuntimeException("Failed to search orders", e);
+//        }
+//    }
+//
+//    public List<OrderDetailDTO> findOrderDetails(String orderId) {
+//        List<OrderDetailDTO> orderDetailsList = new ArrayList<>();
+//
+//        try {
+//            PreparedStatement stm = connection.prepareStatement("SELECT id FROM `order` WHERE id=?");
+//            stm.setString(1, orderId);
+//
+//            if (!stm.executeQuery().next()) throw new RuntimeException("Invalid order id");
+//
+//            stm = connection.prepareStatement("SELECT * FROM order_detail WHERE order_id=?");
+//            stm.setString(1, orderId);
+//            ResultSet rst = stm.executeQuery();
+//
+//            while (rst.next()) {
+//                orderDetailsList.add(new OrderDetailDTO(rst.getString("item_code"),
+//                        rst.getInt("qty"),
+//                        rst.getBigDecimal("unit_price")));
+//            }
+//
+//            return orderDetailsList;
+//        } catch (SQLException e) {
+//            throw new RuntimeException("Failed fetch order details for order id: " + orderId, e);
+//        }
+//    }
+//
+//    public String getLastOrderId() {
+//        try {
+//            ResultSet rst = connection.createStatement().executeQuery("SELECT id FROM `order` ORDER BY id DESC LIMIT 1;");
+//
+//            if (rst.next()) {
+//                return rst.getString("id");
+//            } else {
+//                return null;
+//            }
+//        } catch (Exception e) {
+//            throw new RuntimeException("Failed operation");
+//        }
+//    }
 }
--- src/main/java/lk/ijse/dep7/pos/dao/OrderDetailDAO.java
+++ src/main/java/lk/ijse/dep7/pos/dao/OrderDetailDAO.java
@@ -1,4 +1,39 @@
 package lk.ijse.dep7.pos.dao;
 
+import lk.ijse.dep7.pos.entity.OrderDetail;
+import lk.ijse.dep7.pos.entity.OrderDetailPK;
+
+import java.util.List;
+
 public class OrderDetailDAO {
+
+    public void saveOrderDetail(OrderDetail orderDetail) {
+
+    }
+
+    public void updateOrderDetail(OrderDetail orderDetail) {
+
+    }
+
+    public void deleteOrderDetailByPK(OrderDetailPK orderDetailPK) {
+
+    }
+
+    public OrderDetail findOrderDetailByPK(OrderDetailPK orderDetailPK) {
+        return null;
+    }
+
+    public List<OrderDetail> findAllOrderDetails() {
+        return null;
+    }
+
+    public long countOrderDetails() {
+        return 0;
+    }
+
+    public boolean existsOrderDetailByPK(OrderDetailPK orderDetailPK) {
+        return false;
+    }
+
+
 }
