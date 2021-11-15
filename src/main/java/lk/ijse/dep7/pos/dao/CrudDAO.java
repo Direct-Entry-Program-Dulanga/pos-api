8cfb1298c1537bcd48434f25125936cb51175f16
Ranjith Suranga <d.m.ranjith.sura@gmail.com>
2021-11-15 14:29:47 +0530

    Revamp CrudDAO with Generics

--- src/main/java/lk/ijse/dep7/pos/dao/CrudDAO.java
+++ src/main/java/lk/ijse/dep7/pos/dao/CrudDAO.java
@@ -2,25 +2,26 @@ package lk.ijse.dep7.pos.dao;
 
 import lk.ijse.dep7.pos.entity.SuperEntity;
 
+import java.io.Serializable;
 import java.util.List;
 import java.util.Optional;
 
-public interface CrudDAO {
+public interface CrudDAO<T extends SuperEntity, ID extends Serializable> {
 
-    void save(SuperEntity entity) throws Exception;
+    void save(T entity) throws Exception;
 
-    void update(SuperEntity entity) throws Exception;
+    void update(T entity) throws Exception;
 
-    void deleteById(Object key) throws Exception;
+    void deleteById(ID key) throws Exception;
 
-    Optional<SuperEntity> findById(Object key) throws Exception;
+    Optional<T> findById(ID key) throws Exception;
 
-    List<SuperEntity> findAll() throws Exception;
+    List<T> findAll() throws Exception;
 
     long count() throws Exception;
 
-    boolean existsById(Object key) throws Exception;
+    boolean existsById(ID key) throws Exception;
 
-    List<SuperEntity> findAll(int page, int size) throws Exception;
+    List<T> findAll(int page, int size) throws Exception;
 
 }
