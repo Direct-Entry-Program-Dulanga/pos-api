c11625031a195c99b367dfc1b9b0fa3c61f17646
Ranjith Suranga <d.m.ranjith.sura@gmail.com>
2021-11-16 12:14:52 +0530

    Finalize the ServiceFactory

--- src/main/java/lk/ijse/dep7/pos/service/ServiceFactory.java
+++ src/main/java/lk/ijse/dep7/pos/service/ServiceFactory.java
@@ -1,5 +1,9 @@
 package lk.ijse.dep7.pos.service;
 
+import lk.ijse.dep7.pos.service.custom.impl.CustomerServiceImpl;
+import lk.ijse.dep7.pos.service.custom.impl.ItemServiceImpl;
+import lk.ijse.dep7.pos.service.custom.impl.OrderServiceImpl;
+
 public class ServiceFactory {
 
     private static ServiceFactory serviceFactory;
@@ -10,6 +14,19 @@ public class ServiceFactory {
 
     public static ServiceFactory getInstance() {
         return (serviceFactory == null)? (serviceFactory = new ServiceFactory()): serviceFactory;
+    }
+
+    public <T extends SuperService> T getService(ServiceType serviceType){
+        switch (serviceType){
+            case CUSTOMER:
+                return (T) new CustomerServiceImpl();
+            case ITEM:
+                return (T) new ItemServiceImpl();
+            case ORDER:
+                return (T) new OrderServiceImpl();
+            default:
+                throw new RuntimeException("Invalid service");
+        }
     }
 
 }
