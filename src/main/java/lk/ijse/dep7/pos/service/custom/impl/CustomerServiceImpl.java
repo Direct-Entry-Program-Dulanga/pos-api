d614244f6415d5ec92289cf14809c59fc2415a66
Ranjith Suranga <d.m.ranjith.sura@gmail.com>
2021-11-16 11:55:46 +0530

    Revamp CustomerService with CustomerServiceImpl

--- src/main/java/lk/ijse/dep7/pos/service/custom/impl/CustomerServiceImpl.java
+++ src/main/java/lk/ijse/dep7/pos/service/custom/impl/CustomerServiceImpl.java
@@ -4,12 +4,13 @@ import lk.ijse.dep7.pos.dao.DAOFactory;
 import lk.ijse.dep7.pos.dao.DAOType;
 import lk.ijse.dep7.pos.dao.custom.CustomerDAO;
 import lk.ijse.dep7.pos.dto.CustomerDTO;
+import lk.ijse.dep7.pos.service.custom.CustomerService;
 
 import java.util.List;
 
 import static lk.ijse.dep7.pos.service.util.EntityDTOMapper.*;
 
-public class CustomerServiceImpl {
+public class CustomerServiceImpl implements CustomerService {
 
     private final CustomerDAO customerDAO;
 
@@ -17,6 +18,7 @@ public class CustomerServiceImpl {
         customerDAO = DAOFactory.getInstance().getDAO(DAOType.CUSTOMER);
     }
 
+    @Override
     public void saveCustomer(CustomerDTO customer) throws Exception {
         if (existCustomer(customer.getId())) {
             throw new RuntimeException(customer.getId() + " already exists");
@@ -24,14 +26,17 @@ public class CustomerServiceImpl {
         customerDAO.save(fromCustomerDTO(customer));
     }
 
+    @Override
     public long getCustomersCount() throws Exception {
         return customerDAO.count();
     }
 
-    boolean existCustomer(String id) throws Exception {
+    @Override
+    public boolean existCustomer(String id) throws Exception {
         return customerDAO.existsById(id);
     }
 
+    @Override
     public void updateCustomer(CustomerDTO customer) throws Exception {
         if (!existCustomer(customer.getId())) {
             throw new RuntimeException("There is no such customer associated with the id " + customer.getId());
@@ -39,6 +44,7 @@ public class CustomerServiceImpl {
         customerDAO.update(fromCustomerDTO(customer));
     }
 
+    @Override
     public void deleteCustomer(String id) throws Exception {
         if (!existCustomer(id)) {
             throw new RuntimeException("There is no such customer associated with the id " + id);
@@ -46,18 +52,22 @@ public class CustomerServiceImpl {
         customerDAO.deleteById(id);
     }
 
+    @Override
     public CustomerDTO findCustomer(String id) throws Exception {
         return toCustomerDTO(customerDAO.findById(id).orElseThrow(() -> new RuntimeException("There is no such customer associated with the id " + id)));
     }
 
+    @Override
     public List<CustomerDTO> findAllCustomers() throws Exception {
         return toCustomerDTOList(customerDAO.findAll());
     }
 
+    @Override
     public List<CustomerDTO> findAllCustomers(int page, int size) throws Exception {
         return toCustomerDTOList(customerDAO.findAll(page, size));
     }
 
+    @Override
     public String generateNewCustomerId() throws Exception {
         String id = customerDAO.getLastCustomerId();
         if (id != null) {
